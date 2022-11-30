adec1873883c57d900aa0e9e9341b47ae1a9b5ef
shubhamtunwal <76933039+shubhamtunwal@users.noreply.github.com>
2022-11-30 18:44:42 +0100

    Making Pip installable

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+
+setup(
+    name='snowflake',
+    version='dev',
+
+    url='https://github.com/shubhamtunwal97/DSSSassignment',
+    author='Shubham Tunwal',
+    author_email='shubhamtunwal97@gmail.com',
+
+    py_modules=['snowflake'],
+)
